5de278fd00c784984aa3149e2547fee73c18af06
Marion223 <Jamarioncoleman171@gmail.com>
2024-07-22 21:51:42 +0000

    complete

---
+++ apps.js
@@ -0,0 +1,59 @@
+ "use strict"
+const input = document.querySelector('.input')
+const result =  document.querySelector(".result")
+const deleteBtn = document.querySelector('.delete')
+const keys =document.querySelector('.bottom span')
+
+let operation = ""
+let answer
+let decimalAdded = false
+
+const operators = ['+', '-', 'x', '/']
+
+function handlekeypress(e){
+    const key = e.target.dataset.Key
+    const lastchar = operation[operation.length - 1]
+
+    if (key === '='){
+        return
+    }    
+
+    if (key === '.' && decimalAdded){
+        return
+    }    
+
+    if(operators.indexOf(key) !== -1){
+       decimalAdded = false
+    }
+
+    if (operation.length === 0 && key === '='){
+        operation += key
+        input.innerHTML = operation
+        return
+    }
+
+        if (operation.length === 0 && operators.indexOf(key) !== -1){
+            input.innerHTML = operation
+            return
+        }
+        if (operators.indexOf(lastchar) !== -1 && operators.indexOf(key)){
+            operation = operation.replace(/.$/, key)
+            input.innerHTML = operation
+            return
+        }
+
+       if (key){
+        if(key === '.') decimalAdded = true 
+        operation += key
+        input.innerHTML = operation 
+        return
+       }
+
+}
+
+keys.forEach(key => {
+    key.addEventListener('click', handlekeypress)
+    
+})  
+           
+    
